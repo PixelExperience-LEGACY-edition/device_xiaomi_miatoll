69a44ac1dfe16bffa202befbe64710741f571dd1
COSMIC <rayavanindra@gmail.com>
2024-09-21 09:01:02 +0000

    miatoll: Added Misys [1/2]
    * Required for watermark support with MiuiCamera
    
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,6 +186,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-05-01
 
 # Sepolicy
+include $(DEVICE_PATH)/sepolicy/misys/SEPolicy.mk
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
