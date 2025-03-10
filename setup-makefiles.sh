682ff2eec08272e09fd105b8b57302fc83ae68ea
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-03-10 20:24:26 +0530

    miatoll: setup-makefiles: Add a logic to cleanup unlisted blobs
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -32,5 +32,27 @@ write_headers
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
+# Clean up unlisted blobs
+PROP_DIR="${ANDROID_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary"
+if [ -d "${PROP_DIR}" ]; then
+    # Extract the list of expected files (ignoring comments and blank lines)
+    EXPECTED_FILES=$(grep -vE '(^#|^$)' "${MY_DIR}/proprietary-files.txt" | awk '{print $1}' | sed 's/^-//')
+
+    # Find all proprietary files
+    find "${PROP_DIR}" -type f | while read -r FILE; do
+        REL_PATH="${FILE#${PROP_DIR}/}"
+        FILE_NAME="$(basename "${REL_PATH}")"
+
+        # Ignore .part files silently
+        [[ "${FILE_NAME}" == *.part* ]] && continue
+
+        # Remove unlisted files
+        if ! grep -qF "${REL_PATH}" <<< "${EXPECTED_FILES}"; then
+            echo "Removing unlisted blob: ${FILE}"
+            rm -f "${FILE}"
+        fi
+    done
+fi
+
 # Finish
 write_footers
