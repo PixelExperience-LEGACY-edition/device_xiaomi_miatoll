91786e91dc62d92e8a94f667cea862ae47d5a82b
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-03-14 00:04:16 +0530

    Revert "miatoll: audio: utilize `PCM_24_BIT_PACKED` for speaker"
    This reverts commit 356d4327123d101334c84997a3d095aeede71784.

--- atoll.mk
+++ atoll.mk
@@ -27,10 +27,12 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
+    audio.primary.atoll \
     audio.r_submix.default \
     audio.usb.default \
 
 PRODUCT_PACKAGES += \
+    liba2dpoffload \
     libbatterylistener \
     libcomprcapture \
     libexthwplugin \
@@ -41,6 +43,7 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libsndmonitor \
     libspkrprot \
+    libssrec \
     libvolumelistener \
     sound_trigger.primary.atoll:32
 
@@ -48,6 +51,7 @@ PRODUCT_COPY_FILES += \
      $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
