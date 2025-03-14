a0a92f0f10f00c24665d8c4ed61289c32542bb4a
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-03-15 02:34:06 +0530

    miatoll: Merge to py extract_util script
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils_22 python3
+#
+# SPDX-FileCopyrightText: 2024-2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    BlobFixupCtx,
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
+
+namespace_imports = [
+    'device/xiaomi/miatoll',
+    'hardware/qcom-caf/sm8150',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+
+    ): lib_fixup_vendor_suffix,
+    (
+
+    ): lib_fixup_remove,
+}
+
+
+def blob_fixup_return_1(
+    ctx: BlobFixupCtx,
+    file: File,
+    file_path: str,
+    symbol: str,
+    *args,
+    **kwargs,
+):
+    for line in run_cmd(
+        [
+            llvm_objdump_path,
+            '--dynamic-syms',
+            file_path,
+        ]
+    ).splitlines():
+        if line.endswith(f' {symbol}'):
+            offset, _ = line.split(maxsplit=1)
+
+            with open(file_path, 'rb+') as f:
+                f.seek(int(offset, 16))
+                f.write(b'\x01\x00\xa0\xe3')  # mov r0, #1
+                f.write(b'\x1e\xff\x2f\xe1')  # bx lr
+
+            break
+
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/libnfc-nci.conf': blob_fixup()
+        .regex_replace('/data/nfc', '/data/vendor/nfc'),
+    'vendor/etc/camera/camxoverridesettings.txt': blob_fixup()
+        .regex_replace('0x10082', '0')
+        .regex_replace('0x1F', '0x0'),
+    'vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc': blob_fixup()
+        .regex_replace('@4.0', '@4.1'),
+    'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+    'vendor/lib64/hw/fingerprint.fpc.default.so': blob_fixup()
+        .sig_replace('30 00 00 90 11 3A 42 F9', '30 00 00 90 1F 20 03 D5'),
+    'system_ext/etc/init/wfdservice.rc': blob_fixup()
+        .regex_replace(r'(start|stop) wfdservice\b', r'\1 wfdservice64'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .remove_needed('android.hidl.base@1.0.so'),
+    ### Miui Camera fix Start ###
+    ('system/lib64/libcamera_algoup_jni.xiaomi.so',
+    'system/lib64/libcamera_mianode_jni.xiaomi.so'): blob_fixup()
+        .add_needed('libgui_shim_miuicamera.so'),
+    ### Miui Camera fix End ###
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'miatoll',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
